533a84d26b0f7d1a75c9a9c4fd3917671f8dfd7b
Maciej Sadowski <sm44484@zut.edu.pl>
2022-01-22 12:13:06 +0100

    stats endpoint added, working

--- backend/src/Controller/AdminPanelController.php
+++ backend/src/Controller/AdminPanelController.php
@@ -7,7 +7,9 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Service\ExampleService;
-use App\Service\StatsService;
+use PDO;
+use Doctrine\DBAL\Connection;
+use \Datetime;
 
 /**
  * @Route("/adminpanel", name="adminpanel_")
@@ -16,10 +18,8 @@ class AdminPanelController extends AbstractController
 {
     protected ExampleService $myService;
 
-    protected StatsService $statsService;
 
-    public function __construct(StatsService $statsService, ExampleService $myService) {
-        $this->statsService = $statsService;
+    public function __construct(ExampleService $myService) {
         $this->myService = $myService;
     }
 
@@ -43,10 +43,67 @@ class AdminPanelController extends AbstractController
     /**
      * @Route("/stats", name="stats")
      */
-    public function getStats(): JsonResponse
+    public function getStats(Connection $connection): JsonResponse
     {
-        // return new JsonResponse($this->statsService->getStats());
-        return $this->json($this->statsService->getStats());
+        $stats = $connection->fetchAllAssociative('SELECT start, stop FROM worklog;');
+
+        $today = new DateTime('NOW');
+        $todayDay = $today->format('j');
+        $todayMonth = $today->format('n');
+        $todayYear = $today->format('Y');
+
+        $dayStats = 0;
+        $weekStats = 0;
+        $monthStats = 0;
+        $yearStats = 0;
+
+
+
+        foreach($stats as $stat)
+        {
+            $hour = (int)substr($stat['stop'], 11) - (int)substr($stat['start'], 11);
+            $minute = (int)substr($stat['stop'], 14) - (int)substr($stat['start'], 14);
+            $second = (int)substr($stat['stop'], 17) - (int)substr($stat['start'], 17);
+
+
+            if((string)(int)substr($stat['stop'], 8) == $todayDay && (string)(int)substr($stat['stop'], 5) == $todayMonth)
+            {
+                $dayStats += $second;
+                $dayStats += $minute * 60;
+                $dayStats += $hour * 3600;
+            }
+            elseif (($todayDay - (string)(int)substr($stat['stop'], 8)) <= 7)
+            {
+                $weekStats += $second;
+                $weekStats += $minute * 60;
+                $weekStats += $hour * 3600;
+            }
+            
+            if ((string)(int)substr($stat['stop'], 5) == $todayMonth)
+            {
+                $monthStats += $second;
+                $monthStats += $minute * 60;
+                $monthStats += $hour * 3600;
+            } 
+
+            if ((string)(int)substr($stat['stop'], 0) == $todayYear)
+            {
+                $yearStats += $second;
+                $yearStats += $minute * 60;
+                $yearStats += $hour * 3600;
+            } 
+
+            
+            
+        }
+        return $this->json(
+            [
+                "dayStats" => $dayStats,
+                "weekStats" => $weekStats,
+                "monthStats" => $monthStats,
+                "yearStats" => $yearStats,
+            ]
+    );
     }
 
 }

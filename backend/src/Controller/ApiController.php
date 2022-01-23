4b247a8be303a3fd212dfc9ef6f98fa8bfe01ae5
Maciej Sadowski <sm44484@zut.edu.pl>
2022-01-23 14:44:29 +0100

    small fixes in users

--- backend/src/Controller/ApiController.php
+++ backend/src/Controller/ApiController.php
@@ -5,19 +5,19 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\User\UserInterface;
 use App\Service\UsersService;
+use PDO;
+
+use Doctrine\DBAL\Connection;
 
 class ApiController extends AbstractController
 {
-    public function __construct(UsersService $usersService) {
-       $this->usersService = $usersService;
-    }
 
     /**
      * @Route("/api", methods={"GET"})
      */
-    public function handleGetApi()
+    public function handleGetApi(Connection $connection)
     {
-        $data = ['user' => $this->usersService->mygetUser()];
+        $data = ['user' => $connection->fetchAllAssociative('SELECT * FROM users WHERE id ='.$id.';')];
 
         return $this->json($data, 200);
     }

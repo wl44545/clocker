f888c2c3dffaff8874fdff134f9b8b5a0506014f
Maciej Sadowski <sm44484@zut.edu.pl>
2022-01-23 14:49:31 +0100

    even smaller fixes

--- backend/src/Controller/ApiController.php
+++ backend/src/Controller/ApiController.php
@@ -4,29 +4,14 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\User\UserInterface;
-<<<<<<< Updated upstream
 use App\Service\UsersService;
-<<<<<<< HEAD
+
 use PDO;
 
 use Doctrine\DBAL\Connection;
 
 class ApiController extends AbstractController
 {
-=======
-=======
->>>>>>> Stashed changes
-
-class ApiController extends AbstractController
-{
-    public function __construct(UsersService $usersService) {
-<<<<<<< Updated upstream
-       $this->usersService = $usersService;
-=======
-       $this->UsersService = $usersService;
->>>>>>> Stashed changes
-    }
->>>>>>> 80e15011eb1015ece01d457be5eb2e5edac7e0dc
 
     /**
      * @Route("/api", methods={"GET"})
@@ -34,7 +19,6 @@ class ApiController extends AbstractController
     public function handleGetApi(Connection $connection)
     {
         $data = ['user' => $connection->fetchAllAssociative('SELECT * FROM users WHERE id ='.$id.';')];
-
         return $this->json($data, 200);
     }
 }

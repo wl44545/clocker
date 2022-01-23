80e15011eb1015ece01d457be5eb2e5edac7e0dc
granatejro <migos432@gmail.com>
2022-01-23 14:39:03 +0100

    próba implementacji jwt

--- backend/src/Controller/ApiController.php
+++ backend/src/Controller/ApiController.php
@@ -4,12 +4,19 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\User\UserInterface;
+<<<<<<< Updated upstream
 use App\Service\UsersService;
+=======
+>>>>>>> Stashed changes
 
 class ApiController extends AbstractController
 {
     public function __construct(UsersService $usersService) {
+<<<<<<< Updated upstream
        $this->usersService = $usersService;
+=======
+       $this->UsersService = $usersService;
+>>>>>>> Stashed changes
     }
 
     /**
